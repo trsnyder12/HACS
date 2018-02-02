3aa62c75567868899224d50fb18bdcac178aa2e1
Giovanni Gaito <gtgaito@gmail.com>
2018-02-02 14:19:08 -0500

    Added Get and Put
    Added a get user option for api.
    Also added a put method for any user "attribute"

--- API/serveAPI.py
+++ API/serveAPI.py
@@ -1,6 +1,12 @@
 #where the flask API is going to go
 
 from flask import Flask, jsonify
+from firebase import firebase
+
+
+firebase = firebase.FirebaseApplication('https://hacs-9caa0.firebaseio.com/')
+result = firebase.get('/users',None)
+print (result)
 
 app = Flask(__name__)
 
@@ -8,6 +14,17 @@ app = Flask(__name__)
 def get_example(id):
     ex = id
     return jsonify({'example': ex})
+
+@app.route('/users', methods=['GET'])
+def get_user(username):
+    wantedUser = firebase.get('/users/' + username,None)
+    return jsonify({'user':wantedUser})
+
+@app.route('/users',methods=['PUT'])
+def put_user_attribute(attributeName,attribute):
+    response = firebase.put('/users/'+ attributeName,'value',attribute )
+    print(response)
+    return jsonify({'attribute':response})
 
 if __name__ == '__main__':
     app.run(debug=True)
