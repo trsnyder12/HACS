ed676a49b65404d24d416ed5dc2e1799e992d604
jssomerville <jssomerville2@gmail.com>
2018-04-10 11:35:12 -0400

    more javascript stuff

--- GUI v3/js/hacs.js
+++ GUI v3/js/hacs.js
@@ -1,56 +1,171 @@
-var requestDomainName = 'api.bartrug.me/';
+var requestDomainName = 'http://api.bartrug.me/';
 
-function getEventsByDate(deviceName, month, day, year) {
+function getEventsByDate(deviceName, month, day, year) 
+{
     var requestPath = 'get-events-by-date/';
+    var requestURL = requestDomainName + requestPath + deviceName + '-' + month + day + year;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getEventsByDate(' + deviceName + '-' + month + day + year + ')');
+    }
 
 }
 
-function getEventsByMonth(deviceName, month) {
+function getEventsByMonth(deviceName, month) 
+{
     var requestPath = 'get-events-for-month/';
+    var requestURL = requestDomainName + requestPath + deviceName + '-' + month;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getEventsByMonth(' + deviceName  + '-' + month + ')');
+    }
 
 }
 
-function getUser(username) {
+function getUser(username) 
+{
     var requestPath = 'users/';
+    var requestURL = requestDomainName + requestPath + username;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getUser(' + username  + ')');
+    }
 
 }
 
-function getUsers() {
+function getUsers() 
+{
     var requestPath = 'users/';
+    var requestURL = requestDomainName + requestPath;
 
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+        var users = r.response;
+
+        var usernames = [];
+        for (var key in users.users)
+        {
+            usernames.push(users.users[key].username);
+        }
+
+        return usernames;
+        
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getUser()');
+    }
 }
 
-function getDeviceAttribute(deviceName) {
+function getDeviceAttribute(deviceName) 
+{
     var requestPath = 'devices-get-currData/';
+    var requestURL = requestDomainName + requestPath + deviceName;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getDeviceAttribute(' + deviceName  + ')');
+    }
 
 }
 
-function putsUser() {
+function putsUser() 
+{
     var requestPath = 'post-user/';
 
 }
 
-function putsUserAttribute() {
+function putsUserAttribute() 
+{
     var requestPath = 'post-user-attribute/';
 
 }
 
-function getDevice(deviceName) {
+function getDevice(deviceName) 
+{
     var requestPath = 'get-device/';
+    var requestURL = requestDomainName + requestPath + deviceName;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getDeviceWS(' + deviceName  + ')');
+    }
 
 }
 
-function putsDevice() {
+function putsDevice() 
+{
     var requestPath = 'post-device/';
 
 }
 
-function deleteDevice(deviceName) {
+function deleteDevice(deviceName) 
+{
     var requestPath = 'delete-device/';
 
 }
 
-function deleteUser(username) {
+function deleteUser(username) 
+{
     var requestPath = 'delete-user/';
 
 }
