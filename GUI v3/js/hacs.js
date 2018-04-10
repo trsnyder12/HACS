31df2bcacba4575370f063ee946e659ee589933b
jssomerville <jssomerville2@gmail.com>
2018-04-10 08:54:07 -0400

    added prototypes of javascript functions

---
+++ GUI v3/js/hacs.js
@@ -0,0 +1,56 @@
+var requestDomainName = 'api.bartrug.me/';
+
+function getEventsByDate(deviceName, month, day, year) {
+    var requestPath = 'get-events-by-date/';
+
+}
+
+function getEventsByMonth(deviceName, month) {
+    var requestPath = 'get-events-for-month/';
+
+}
+
+function getUser(username) {
+    var requestPath = 'users/';
+
+}
+
+function getUsers() {
+    var requestPath = 'users/';
+
+}
+
+function getDeviceAttribute(deviceName) {
+    var requestPath = 'devices-get-currData/';
+
+}
+
+function putsUser() {
+    var requestPath = 'post-user/';
+
+}
+
+function putsUserAttribute() {
+    var requestPath = 'post-user-attribute/';
+
+}
+
+function getDevice(deviceName) {
+    var requestPath = 'get-device/';
+
+}
+
+function putsDevice() {
+    var requestPath = 'post-device/';
+
+}
+
+function deleteDevice(deviceName) {
+    var requestPath = 'delete-device/';
+
+}
+
+function deleteUser(username) {
+    var requestPath = 'delete-user/';
+
+}
