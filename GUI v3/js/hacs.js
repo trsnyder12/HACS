895c43c62fa734795ca3b893113c17c6358d4565
jssomerville <jssomerville2@gmail.com>
2018-04-16 08:06:41 -0400

    some api changes

--- GUI v3/js/hacs.js
+++ GUI v3/js/hacs.js
@@ -13,6 +13,114 @@ function getEventsByDate(deviceName, month, day, year)
     r.onload = function() 
     {
 
+/*
+        Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+        Chart.defaults.global.defaultFontColor = '#292b2c';
+        // -- Area Chart Example
+        var ctx = document.getElementById("deviceChart");
+        var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+            datasets: [{
+              label: "Sessions",
+              lineTension: 0.3,
+              backgroundColor: "rgba(2,117,216,0.2)",
+              borderColor: "rgba(2,117,216,1)",
+              pointRadius: 5,
+              pointBackgroundColor: "rgba(2,117,216,1)",
+              pointBorderColor: "rgba(255,255,255,0.8)",
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(2,117,216,1)",
+              pointHitRadius: 20,
+              pointBorderWidth: 2,
+              data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+            }],
+          },
+          options: {
+            scales: {
+              xAxes: [{
+                time: {
+                  unit: 'date'
+                },
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  maxTicksLimit: 7
+                }
+              }],
+              yAxes: [{
+                ticks: {
+                  min: 0,
+                  max: 40000,
+                  maxTicksLimit: 5
+                },
+                gridLines: {
+                  color: "rgba(0, 0, 0, .125)",
+                }
+              }],
+            },
+            legend: {
+              display: false
+            }
+          }
+        });
+        // -- Bar Chart Example
+        var ctx = document.getElementById("myBarChart");
+        var myLineChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [{
+              label: "Revenue",
+              backgroundColor: "rgba(2,117,216,1)",
+              borderColor: "rgba(2,117,216,1)",
+              data: [4215, 5312, 6251, 7841, 9821, 14984],
+            }],
+          },
+          options: {
+            scales: {
+              xAxes: [{
+                time: {
+                  unit: 'month'
+                },
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  maxTicksLimit: 6
+                }
+              }],
+              yAxes: [{
+                ticks: {
+                  min: 0,
+                  max: 15000,
+                  maxTicksLimit: 5
+                },
+                gridLines: {
+                  display: true
+                }
+              }],
+            },
+            legend: {
+              display: false
+            }
+          }
+        });
+        // -- Pie Chart Example
+        var ctx = document.getElementById("myPieChart");
+        var myPieChart = new Chart(ctx, {
+          type: 'pie',
+          data: {
+            labels: ["Blue", "Red", "Yellow", "Green"],
+            datasets: [{
+              data: [12.21, 15.58, 11.25, 8.32],
+              backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+            }],
+          },
+        });
+        **/
     }
 
     r.onerror = function() 
@@ -34,7 +142,12 @@ function getEventsByMonth(deviceName, month)
 
     r.onload = function() 
     {
+        data = r.response;
 
+        for (var key in data)
+        {
+            console.log(key);
+        }
     }
 
     r.onerror = function() 
@@ -70,12 +183,11 @@ function getUsers()
 {
     var requestPath = 'users/';
     var requestURL = requestDomainName + requestPath;
-    console.log("testing");
     var r = new XMLHttpRequest();
     r.open('GET', requestURL);
     r.responseType = 'json';
     r.send();
-    console.log("got the stuff");
+
     r.onload = function() 
     {
         var users = r.response;
@@ -182,7 +294,51 @@ function getDevices()
 
     r.onload = function() 
     {
-        alert(1);
+        devInfo = document.getElementById("devInfo");
+        devices = r.response;
+
+        var names = []
+        for (var key in devices.devices)
+        {
+            var tr = document.createElement("tr");
+            var td = document.createElement("td");
+            var txt = document.createTextNode(key);
+
+            for (var i in devices.devices[key])
+            {
+                if (typeof devices.devices[key][i] === "string")
+                {
+                    td = document.createElement("td");
+                
+                    txt = document.createTextNode(devices.devices[key][i]);
+                    td.appendChild(txt);    
+                    tr.appendChild(td); 
+                }
+            }
+
+            var div = document.createElement("div");
+            txt = document.createTextNode("Delete");
+            div.appendChild(txt);
+            div.classList.add('btn');
+            div.classList.add('btn-danger');
+            div.classList.add('table-btn');
+            td = document.createElement("td");
+            td.appendChild(div);
+            tr.appendChild(td);
+
+            div = document.createElement("div");
+            txt = document.createTextNode("Edit");
+            div.appendChild(txt);
+            div.classList.add('btn');
+            div.classList.add('btn-info');
+            div.classList.add('table-btn');
+            td = document.createElement("td");
+            td.appendChild(div);
+            tr.appendChild(td);
+
+            devInfo.appendChild(tr);
+        }   
+        
     }
 
     r.onerror = function() 
