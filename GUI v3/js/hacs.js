65bfb4d2b2620fe503cbd083cf45ffa5f9747759
jssomerville <jssomerville2@gmail.com>
2018-04-16 12:37:31 -0400

    finished graphs

--- GUI v3/js/hacs.js
+++ GUI v3/js/hacs.js
@@ -13,17 +13,45 @@ function getEventsByDate(deviceName, month, day, year)
     r.onload = function() 
     {
 
-/*
-        Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getEventsByDate(' + deviceName + '-' + month + day + year + ')');
+    }
+
+}
+
+function populateChart(deviceName, month, value, diction, time)
+{   
+    Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
         Chart.defaults.global.defaultFontColor = '#292b2c';
-        // -- Area Chart Example
+
         var ctx = document.getElementById("deviceChart");
+        ctx.remove();
+
+        var con = document.getElementById("con");
+
+        var can = document.createElement("canvas");
+        can.setAttribute("id", "deviceChart");
+        can.setAttribute("width", "100%");
+        can.setAttribute("height", "30");
+        con.appendChild(can);
+
+        ctx = document.getElementById('deviceChart');
+
+        /*
+        <div class="card-body">
+            <canvas id="deviceChart" width="100%" height="30"></canvas>
+        </div>
+        **/
+
         var myLineChart = new Chart(ctx, {
           type: 'line',
           data: {
-            labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+            labels: time,
             datasets: [{
-              label: "Sessions",
+              label: value,
               lineTension: 0.3,
               backgroundColor: "rgba(2,117,216,0.2)",
               borderColor: "rgba(2,117,216,1)",
@@ -34,7 +62,7 @@ function getEventsByDate(deviceName, month, day, year)
               pointHoverBackgroundColor: "rgba(2,117,216,1)",
               pointHitRadius: 20,
               pointBorderWidth: 2,
-              data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+              data: diction[value],
             }],
           },
           options: {
@@ -52,8 +80,8 @@ function getEventsByDate(deviceName, month, day, year)
               }],
               yAxes: [{
                 ticks: {
-                  min: 0,
-                  max: 40000,
+                  min: Math.min.apply(null, (diction[value])) - 10,
+                  max: Math.max.apply(null, (diction[value])) + 10,
                   maxTicksLimit: 5
                 },
                 gridLines: {
@@ -66,67 +94,6 @@ function getEventsByDate(deviceName, month, day, year)
             }
           }
         });
-        // -- Bar Chart Example
-        var ctx = document.getElementById("myBarChart");
-        var myLineChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: ["January", "February", "March", "April", "May", "June"],
-            datasets: [{
-              label: "Revenue",
-              backgroundColor: "rgba(2,117,216,1)",
-              borderColor: "rgba(2,117,216,1)",
-              data: [4215, 5312, 6251, 7841, 9821, 14984],
-            }],
-          },
-          options: {
-            scales: {
-              xAxes: [{
-                time: {
-                  unit: 'month'
-                },
-                gridLines: {
-                  display: false
-                },
-                ticks: {
-                  maxTicksLimit: 6
-                }
-              }],
-              yAxes: [{
-                ticks: {
-                  min: 0,
-                  max: 15000,
-                  maxTicksLimit: 5
-                },
-                gridLines: {
-                  display: true
-                }
-              }],
-            },
-            legend: {
-              display: false
-            }
-          }
-        });
-        // -- Pie Chart Example
-        var ctx = document.getElementById("myPieChart");
-        var myPieChart = new Chart(ctx, {
-          type: 'pie',
-          data: {
-            labels: ["Blue", "Red", "Yellow", "Green"],
-            datasets: [{
-              data: [12.21, 15.58, 11.25, 8.32],
-              backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-            }],
-          },
-        });
-        **/
-    }
-
-    r.onerror = function() 
-    {
-        console.log('There was an error while processing getEventsByDate(' + deviceName + '-' + month + day + year + ')');
-    }
 
 }
 
@@ -142,83 +109,71 @@ function getEventsByMonth(deviceName, month)
 
     r.onload = function() 
     {
+        var del = document.getElementById('tit');
+        if (del != null)
+        {
+            del.remove();
+        }
+
+        var head = document.getElementById("deviceCardHeader");
+        var tit = document.createElement("a");
+        var header = document.createTextNode(deviceName);
+        tit.setAttribute("id", "tit");
+        tit.appendChild(header);
+        head.appendChild(tit);
+
         var data = r.response;
 
         var time = [];
-        var temp = [];
-        var humi = [];
+        var values = [];
+        var diction = {};
 
         for (var obj in data)
         {
             for (var key in data[obj])
             {
-
                 //console.log(key); // Times
                 var d = JSON.parse(data[obj][key].values);
                 //console.log(d.temp); // Temperature
                 //console.log(d.humidity); // Humidity
                 time.push(key);
-                temp.push(d.temp);
-                humi.push(d.humidity);
+
+                //Object.keys(d)
+                values = Object.keys(d);
+
+                for (var i in values)
+                {
+                    if (!(values[i] in diction))
+                    {
+                        diction[values[i]] = [];
+                    }
+                    diction[values[i]].push(d[values[i]]);
+                }
+
+
+
             }
         }
 
 
-        console.log(time);
-        console.log(temp);
-        console.log(humi);
-        Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-        Chart.defaults.global.defaultFontColor = '#292b2c';
 
-        var ctx = document.getElementById("deviceChart");
-        var myLineChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: time,
-            datasets: [{
-              label: "Temperature",
-              lineTension: 0.3,
-              backgroundColor: "rgba(2,117,216,0.2)",
-              borderColor: "rgba(2,117,216,1)",
-              pointRadius: 5,
-              pointBackgroundColor: "rgba(2,117,216,1)",
-              pointBorderColor: "rgba(255,255,255,0.8)",
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "rgba(2,117,216,1)",
-              pointHitRadius: 20,
-              pointBorderWidth: 2,
-              data: temp,
-            }],
-          },
-          options: {
-            scales: {
-              xAxes: [{
-                time: {
-                  unit: 'date'
-                },
-                gridLines: {
-                  display: false
-                },
-                ticks: {
-                  maxTicksLimit: 7
-                }
-              }],
-              yAxes: [{
-                ticks: {
-                  min: -10,
-                  max: 100,
-                  maxTicksLimit: 5
-                },
-                gridLines: {
-                  color: "rgba(0, 0, 0, .125)",
-                }
-              }],
-            },
-            legend: {
-              display: false
-            }
-          }
-        });
+        valDrop = document.getElementById('valuesDrop');
+        valDrop.innerHTML = '';
+        for (var i in values)
+        {
+            let it = values[i];
+            var a = document.createElement("a");
+
+            a.classList.add('dropdown-item');
+            var txt = document.createTextNode(values[i]);
+            a.appendChild(txt);
+            a.addEventListener('click', function(){ 
+                populateChart(deviceName, month, it, diction, time); 
+            });
+            valDrop.appendChild(a);
+
+        }
+
     }
 
     r.onerror = function() 
@@ -331,6 +286,46 @@ function getDevice(deviceName)
     r.onerror = function() 
     {
         console.log('There was an error while processing getDeviceWS(' + deviceName  + ')');
+    }
+
+}
+
+function populateDevicesDropdown()
+{
+    var requestPath = 'devices/';
+    var requestURL = requestDomainName + requestPath;
+
+    var r = new XMLHttpRequest();
+    r.open('GET', requestURL);
+    r.responseType = 'json';
+    r.send();
+
+    r.onload = function() 
+    {
+        var drop = document.getElementById("devicesDrop");
+        devices = r.response;
+
+        var names = []
+        for (var key in devices.devices)
+        {
+            let keyo = key;
+            var a = document.createElement("a");
+
+            a.classList.add('dropdown-item');
+            var txt = document.createTextNode(devices.devices[key].nickname);
+            a.appendChild(txt);
+            a.addEventListener('click', function(){ 
+                getEventsByMonth(keyo, 'April'); 
+            });
+
+            drop.appendChild(a);
+        }   
+        
+    }
+
+    r.onerror = function() 
+    {
+        console.log('There was an error while processing getDevices()');
     }
 
 }
